b118addef2b3607a92c639820cc065563f77850c
Lua Jeon <luajeon506@gmail.com>
2024-11-30 03:42:16 +0900

    init:

---
+++ dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.12
+
+WORKDIR /db_dashboard
+
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    curl \
+    software-properties-common \
+    git \
+    && rm -rf /var/lib/apt/lists/*
+
+COPY requirements.txt ./
+COPY pyplus ./pyplus
+COPY main.py ./
+
+RUN pip install -r requirements.txt
+
+EXPOSE 8042
+
+CMD [ "streamlit", "run", "--server.address=0.0.0.0", "--server.port=8042", "main.py"]
---
+++ main.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import time
+import pyplus.streamlit as stp
+
+st.set_page_config(layout="wide")
+
+hide_streamlit_style='''
+            <style>
+                /* Hide the Streamlit header and menu */
+                header {visibility: hidden;}
+                /* Hide your specific div class, replace class name with the one you identified */
+                .st-emotion-cache-1jicfl2 {
+                    padding: 0rem 0rem 0rem;
+                }
+                .st-emotion-cache-1eup5c7 {
+                    gap: 0rem;
+                }
+            </style>
+        '''
+
+height_max=200
+
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+class Displayer:
+    pass
+
+with st.container(height=height_max):
+    function_list = [func for func in dir(Displayer) if not func.startswith('_')]
+    tp = stp.TabsPlus(titles=function_list,layout='column')
+    for key in function_list:
+        with tp[key]:
+            getattr(Displayer, key)()
+
+time.sleep(1.0)
+st.rerun()
